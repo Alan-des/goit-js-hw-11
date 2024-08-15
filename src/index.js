07ee04c95db9713279af25c07882c921c1a7c19c
=Alan-des <alanakhundov2@gmail.com>
2024-08-16 00:06:31 +0300

    u

--- src/index.js
+++ src/index.js
@@ -3,47 +3,43 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const BASE_URL = 'https://pixabay.com/api/';
+let pageCounter = 1;
+let lightbox;
+
 const selectors = {
   form: document.querySelector('.search-form'),
   cardDiv: document.querySelector('.gallery'),
   btnMore: document.querySelector('.load-more'),
 };
 
-const BASE_URL = 'https://pixabay.com/api/';
-let pageCounter = 1;
-let lightbox;
-
 selectors.form.addEventListener('submit', formSubmit);
 selectors.btnMore.addEventListener('click', fetchMore);
 
-function formSubmit(e) {
+async function formSubmit(e) {
   e.preventDefault();
   selectors.cardDiv.innerHTML = '';
   const inputValue = e.currentTarget.elements[0].value;
   pageCounter = 1;
-  fetchGetUser(inputValue)
-    .then(({ hits, totalHits }) => {
-      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-      selectors.cardDiv.innerHTML = hits.map(createMarkup).join('');
-      lightbox = new SimpleLightbox('.photo-card a', {
-        /* options */
-      });
+
+try {
+  const { hits, totalHits } = await fetchGetUser(inputValue)
+  selectors.cardDiv.innerHTML = hits.map(createMarkup).join('');
+      lightbox = new SimpleLightbox('.photo-card a', {});
       selectors.btnMore.classList.remove('hidden');
-    })
-    .catch(error => {
-      console.log(error);
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    });
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+} catch (error) {
+  console.log(error);
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.')
+
+}
 }
 
-function fetchGetUser(value) {
-  return axios
-    .get(
-      `${BASE_URL}?key=45402382-bdda8f309a8fc5b626a41cecf&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pageCounter}`
-    )
-    .then(res => res.data);
+async function fetchGetUser(value) {
+  const response = await axios.get(`${BASE_URL}?key=45402382-bdda8f309a8fc5b626a41cecf&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pageCounter}`)
+    return response.data
+    
 }
 
 function createMarkup(hits) {
@@ -76,29 +72,33 @@ function createMarkup(hits) {
 </div>`;
 }
 
-function fetchMore() {
+async function fetchMore() {
   selectors.btnMore.classList.add('hidden');
   const inputValue = selectors.form.elements[0].value;
   pageCounter += 1;
-  fetchGetUser(inputValue)
-    .then(({ hits, totalHits }) => {
-      selectors.cardDiv.insertAdjacentHTML(
-        'beforeend',
-        hits.map(createMarkup).join('')
+
+  try {
+    const { hits, totalHits } = await fetchGetUser(inputValue)
+    selectors.cardDiv.insertAdjacentHTML(
+      'beforeend',
+      hits.map(createMarkup).join('')
+    );
+    lightbox.refresh();
+    selectors.btnMore.classList.remove('hidden');
+    if (pageCounter * hits.length >= totalHits) {
+      selectors.btnMore.classList.add('hidden');
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
       );
-      lightbox.refresh();
-      selectors.btnMore.classList.remove('hidden');
-      if (pageCounter * hits.length >= totalHits) {
-        selectors.btnMore.classList.add('hidden');
-        Notiflix.Notify.info(
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
-    })
-    .catch(error => {
-      console.log(error);
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    });
-}
+    }
+  } catch (error) {
+    console.log(error);
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+
+
+
+
+    }
